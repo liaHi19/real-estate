ba0708ab2bc8880d5808f534f21d18559bcfd51f
liaHi19 <gn.natalia19@gmail.com>
2025-08-21 11:44:47 +0100

    feat: adding FeaturesSection

--- app/(nondashboard)/landing/page.tsx
+++ app/(nondashboard)/landing/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
 
 function Landing() {
   return (
-    <div>
+    <>
       <HeroSection />
-    </div>
+      <FeaturesSection />
+    </>
   );
 }
 
